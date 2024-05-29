c6ad3bf9047c5ab1e5e91de1ec648403484a7fa3
Cod3Uchiha <157640942+Cod3Uchiha@users.noreply.github.com>
2024-05-29 16:42:57 +0200

    Update Styles.js

--- commands/Styles.js
+++ commands/Styles.js
@@ -27,7 +27,7 @@ function list(text, fancy) {
     let msg = '\n*Fancy Disponible:*\n\n';
     for (let style in styles) {
         if (style == 33) {
-            msg += (parseInt(style) + 1) + '. ' + 'GOLD-MD' + '\n';
+            msg += (parseInt(style) + 1) + '. ' + 'Manjiro-Sano-md' + '\n';
         } else {
             msg += (parseInt(style) + 1) + '. ' + fancy.apply(fancy[parseInt(style)], text) + '\n';
         }
