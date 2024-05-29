dd6ae81eb55435ed66d44321868ac5bbe8caf9d1
Cod3Uchiha <157640942+Cod3Uchiha@users.noreply.github.com>
2024-05-29 23:49:15 +0200

    Update Heroku.js

--- commands/Heroku.js
+++ commands/Heroku.js
@@ -1,14 +1,14 @@
 
 /** 
 
- GOLD MD
+ Manjiro-Sano-md
 
   𝗖𝗼𝗽𝘆𝗿𝗶𝗴𝗵𝘁 (𝗖) 2024.
  𝗟𝗶𝗰𝗲𝗻𝘀𝗲𝗱 𝘂𝗻𝗱𝗲𝗿 𝘁𝗵𝗲  𝗠𝗜𝗧 𝗟𝗶𝗰𝗲𝗻𝘀𝗲;
  𝗬𝗼𝘂 𝗺𝗮𝘆 𝗻𝗼𝘁 𝘂𝘀𝗲 𝘁𝗵𝗶𝘀 𝗳𝗶𝗹𝗲 𝗲𝘅𝗰𝗲𝗽𝘁 𝗶𝗻 𝗰𝗼𝗺𝗽𝗹𝗶𝗮𝗻𝗰𝗲 𝘄𝗶𝘁𝗵 𝘁𝗵𝗲 𝗟𝗶𝗰𝗲𝗻𝘀𝗲.
  𝗜𝘁 𝗶𝘀 𝘀𝘂𝗽𝗽𝗹𝗶𝗲𝗱 𝗶𝗻 𝘁𝗵𝗲 𝗵𝗼𝗽𝗲 𝘁𝗵𝗮𝘁 𝗶𝘁 𝗺𝗮𝘆 𝗯𝗲 𝘂𝘀𝗲𝗳𝘂𝗹.
- * @𝗽𝗿𝗼𝗷𝗲𝗰𝘁_𝗻𝗮𝗺𝗲 : GOLD 𝗠𝗗, 𝗮 𝘀𝗶𝗺𝗽𝗹𝗲 𝗮𝗻𝗱 𝗲𝗮𝘀𝘆 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝘂𝘀𝗲𝗿 𝗯𝗼𝘁 
- * @𝗼𝘄𝗻𝗲𝗿: UMAR 
+ * @𝗽𝗿𝗼𝗷𝗲𝗰𝘁_𝗻𝗮𝗺𝗲 : Manjiro-Sano-md, 𝗮 𝘀𝗶𝗺𝗽𝗹𝗲 𝗮𝗻𝗱 𝗲𝗮𝘀𝘆 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝘂𝘀𝗲𝗿 𝗯𝗼𝘁 
+ * @𝗼𝘄𝗻𝗲𝗿: Cod3Uchiha 
  
  **/
 
@@ -36,7 +36,7 @@ france(
        const {ms,repondre,superUser , arg} = commandeOptions ;
        
        if(!superUser){repondre('Yeh command sirf mere lie hai');return};
-       if(!arg[0] || !(arg.join('').split('='))) {repondre('Galat Likha ap ne ; Ese Likho :\nSetvar BOT_NAME=GOLD-MD');return};
+       if(!arg[0] || !(arg.join('').split('='))) {repondre('Galat Likha ap ne ; Ese Likho :\nSetvar BOT_NAME=Manjiro-Sano-md');return};
      
     const text = arg.join(" ")
      const Heroku = require("heroku-client");
@@ -51,7 +51,7 @@ france(
                   [text.split('=')[0]]: text.split('=')[1],
           },
         });
-        await repondre('*_HEROKU VAR UPDATE HO GAYI HAI AB GOLD-MD RESTART HO RAHA HAI...'_*)
+        await repondre('*_HEROKU VAR UPDATE HO GAYI HAI AB Manjiro-Sano-md RESTART HO RAHA HAI...'_*)
     }
 );
 
@@ -73,7 +73,7 @@ france(
 			let baseURI = "/apps/" + s.HEROKU_APP_NAME;
 
             let h = await heroku.get(baseURI+'/config-vars')
-let str = '*🔰 GOLD-MD HEROKU VARS 🔰*\n\n'
+let str = '*🔰 Manjiro-Sano-md HEROKU VARS 🔰*\n\n'
 for (vr in h) {
 str+= '🦋 *'+vr+'* '+'= '+h[vr]+'\n'
 }
