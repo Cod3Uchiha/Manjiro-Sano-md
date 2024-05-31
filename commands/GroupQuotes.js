8c4e32fb4fff8f609f68a37081e9366fdadb6a89
Cod3Uchiha <157640942+Cod3Uchiha@users.noreply.github.com>
2024-05-31 17:30:43 +0200

    Update GroupQuotes.js

--- commands/GroupQuotes.js
+++ commands/GroupQuotes.js
@@ -1,14 +1,5 @@
 /** 
-
-🇫‌🇱‌🇦‌🇸‌🇭‌-🇲‌🇩‌ 
-
-  𝗖𝗼𝗽𝘆𝗿𝗶𝗴𝗵𝘁 (𝗖) 2024.
- 𝗟𝗶𝗰𝗲𝗻𝘀𝗲𝗱 𝘂𝗻𝗱𝗲𝗿 𝘁𝗵𝗲  𝗠𝗜𝗧 𝗟𝗶𝗰𝗲𝗻𝘀𝗲;
- 𝗬𝗼𝘂 𝗺𝗮𝘆 𝗻𝗼𝘁 𝘂𝘀𝗲 𝘁𝗵𝗶𝘀 𝗳𝗶𝗹𝗲 𝗲𝘅𝗰𝗲𝗽𝘁 𝗶𝗻 𝗰𝗼𝗺𝗽𝗹𝗶𝗮𝗻𝗰𝗲 𝘄𝗶𝘁𝗵 𝘁𝗵𝗲 𝗟𝗶𝗰𝗲𝗻𝘀𝗲.
- 𝗜𝘁 𝗶𝘀 𝘀𝘂𝗽𝗽𝗹𝗶𝗲𝗱 𝗶𝗻 𝘁𝗵𝗲 𝗵𝗼𝗽𝗲 𝘁𝗵𝗮𝘁 𝗶𝘁 𝗺𝗮𝘆 𝗯𝗲 𝘂𝘀𝗲𝗳𝘂𝗹.
- * @𝗽𝗿𝗼𝗷𝗲𝗰𝘁_𝗻𝗮𝗺𝗲 : 𝗙𝗹𝗮𝘀𝗵 𝗠𝗗, 𝗮 𝘀𝗶𝗺𝗽𝗹𝗲 𝗮𝗻𝗱 𝗲𝗮𝘀𝘆 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝘂𝘀𝗲𝗿 𝗯𝗼𝘁 
- * @𝗼𝘄𝗻𝗲𝗿: 𝗙𝗿𝗮𝗻𝗰𝗲 𝗞𝗶𝗻𝗴 
- 
+Manjiro-Sano-md
  **/
 
 
@@ -32,16 +23,16 @@ france({ nomCom: 'quote', categorie: 'Group' }, async (dest, zk, commandeOptions
       fetch('https://animechan.xyz/api/random')
         .then((response) => response.json())
         .then(async (quote) => {
-          repondre(`*FLASH-MD Quotes*
+          repondre(`*Manjiro-Sano-md Quotes*
 
 🎬 Anime: ${quote.anime}
 👤 Character: ${quote.character}
 💬 Quote: ${quote.quote}
 
-Powered by *France King*`);
+provided by *Cod3Uchiha*`);
         });
     } catch (e) {
-      repondre('Erreur lors de la génération de la citation : ' + e.message);
+      repondre('Error generating the quote : ' + e.message);
     }
   } else {
     const query = arg.join(' ');
@@ -50,16 +41,16 @@ Powered by *France King*`);
       fetch('https://animechan.xyz/api/random/character?name=' + query)
         .then((response) => response.json())
         .then(async (quote) => {
-          repondre(`FLASH-MD
+          repondre(`Manjiro-Sano-md
 
 🎬 Anime: ${quote.anime}
 👤 Character: ${quote.character}
 💬 Quote: ${quote.quote}
 
-Powered by *FLASH-MD*`);
+Powered by *Manjiro-Sano-md*`);
         });
     } catch (e) {
-      repondre('Erreur lors de la génération de la citation : ' + e.message);
+      repondre('Error during the generation of the quote : ' + e.message);
     }
   }
 });
